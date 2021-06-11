b7f8a302305e816f0b2a968e3a2e91666056f15c
yang <123>
2021-06-11 16:27:25 +0800

    Initial commit

---
+++ src/main/java/com/cp/consumersmq/ConsumersmqApplication.java
@@ -0,0 +1,13 @@
+package com.cp.consumersmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumersmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumersmqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cp/consumersmq/ConsumersmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cp.consumersmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumersmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
