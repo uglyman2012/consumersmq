0435da8e36cdfe7e51cc4eb7f8cb9c646de75b29
yang <123>
2021-06-18 15:35:39 +0800

    消息手动确认

--- src/main/java/com/cp/consumersmq/service/SysPersonalMessageHandler.java
+++ src/main/java/com/cp/consumersmq/service/SysPersonalMessageHandler.java
@@ -2,13 +2,18 @@ package com.cp.consumersmq.service;
 
 import com.alibaba.fastjson.JSON;
 import com.cp.consumersmq.bean.SysPersonalInfo;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * <p>
@@ -23,10 +28,27 @@ import org.springframework.stereotype.Component;
 public class SysPersonalMessageHandler {
     private static Logger log = LoggerFactory.getLogger(SysPersonalMessageHandler.class);
 
-    @StreamListener(value = Processor.INPUT,condition = "headers['payload_simple_name']=='SysPersonalInfoVO'")
-    public void sysPersonalMessageListnner(@Payload SysPersonalInfo sysPersonalInfo) {
+    @StreamListener(value = Processor.INPUT,condition = "headers['payload_simple_name']=='SysPersonalInfoVO2'")
+    public void sysPersonalMessageListnner(@Payload SysPersonalInfo sysPersonalInfo ) {
         log.info("会员订单T逍客mq信息: {}", JSON.toJSONString(sysPersonalInfo));
-        int a=9/0;
+
     }
+    /**
+     * 配置文件需要开启消息确认为手动模式
+     */
+    @StreamListener(value = Processor.INPUT,condition = "headers['payload_simple_name']=='SysPersonalInfoVO'")
+    public void sysPersonalMessageListnner2(@Payload SysPersonalInfo sysPersonalInfo , @Header(AmqpHeaders.CHANNEL) Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) Long DELIVERY_TAG) throws IOException, InterruptedException {
+
+        log.info("会员订单T逍客mq信息: {}", JSON.toJSONString(sysPersonalInfo));
+
+        channel.basicAck(DELIVERY_TAG,false);
+        //channel.basicNack(DELIVERY_TAG,false,false);
+
+    }
+    //@StreamListener("errorChannel")
+    //public void error(Message<?> message) {
+    //    ErrorMessage errorMessage = (ErrorMessage) message;
+    //    System.err.println("Handling ERROR: " + errorMessage);
+    //}
 
 }
