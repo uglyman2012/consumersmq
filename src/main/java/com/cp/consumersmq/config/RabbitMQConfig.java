fabb7226bddd447928c85313c2ecacda8d544cf1
yang <123>
2021-08-05 16:41:19 +0800

    mq连接池

--- src/main/java/com/cp/consumersmq/config/RabbitMQConfig.java
+++ src/main/java/com/cp/consumersmq/config/RabbitMQConfig.java
@@ -29,6 +29,14 @@ public class RabbitMQConfig {
         //connectionFactory.setVirtualHost("/vhost_cp");
         return connectionFactory;
     }
+    //@Bean
+    //public CachingConnectionFactory rabbitConnectionFactory() {
+    //    CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host);
+    //    connectionFactory.setUsername(username);
+    //    connectionFactory.setPassword(password);
+    //    connectionFactory.setPort(port);
+    //    return connectionFactory;
+    //}
 
     //形参名称要与bean的方法名保持一致
     @Bean
@@ -41,6 +49,8 @@ public class RabbitMQConfig {
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        //使用单独的发送连接，避免生产者由于各种原因阻塞而导致消费者同样阻塞
+        rabbitTemplate.setUsePublisherConnection(true);
         return rabbitTemplate;
     }
 }
